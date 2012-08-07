70db5b5596ad002b39a894345c52a45d5068ffa5
zach lieberman <zach@eyebeam.org>
2012-08-06 21:35:47 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ConnectingLight::Application.config.secret_token = '52dfa77e9a65390299c7acc1703775901aab57a169e3adcf04b8ce378d0c4a125ca8256a43f23582e9ea17a821725ee90f84422536f520e0a7577021c35f9b1d'
