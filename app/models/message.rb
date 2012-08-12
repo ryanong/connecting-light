925c75655756865860f916d07af1ad2f5e4a4382
Ryan Ong <ryanong@gmail.com>
2012-08-12 16:32:59 -0400

    new hadrian wall calculator

--- app/models/message.rb
+++ app/models/message.rb
@@ -15,58 +15,13 @@ class Message < ActiveRecord::Base
     }
 
   def self.latest
-    where(status: 'approved').limit(200).order("id DESC")
+    limit(200).order("id DESC")
   end
-
-  WALLSEND = Vector[54.9913,-1.5290]
-  BOWNESS = Vector[54.9532, -3.2146]
-
-  def point
-    Vector[latitude,longitude]
-  end
-
-  def self.distance( point1, point2 )
-    xs = point2[0] - point1[0];
-    xs = xs * xs;
-
-    ys = point2[1] - point1[1];
-    ys = ys * ys;
-
-    return Math.sqrt( xs + ys );
-  end
-
-  HADRIANS_DISTANCE = distance(WALLSEND, BOWNESS)
 
   def calculate_location_on_wall!
     return set_random_location! if latitude.blank? || longitude.blank?
-    dx = WALLSEND[0] - BOWNESS[0]
-    dy = WALLSEND[1] - BOWNESS[1]
-    # Calculate the t that minimizes the distance.
-    t = ((point[0] - BOWNESS[0]) * dx + (point[1] - BOWNESS[1]) * dy) / (dx**2 + dy**2)
 
-    # See if this represents one of the segment's
-    # end points or a point in the middle.
-    if (t < 0)
-      closest_point = BOWNESS
-      dx = point[0] - BOWNESS[0]
-      dy = point[1] - BOWNESS[1]
-    elsif (t > 1)
-      closest_point = WALLSEND
-      dx = point[0] - WALLSEND[0]
-      dy = point[1] - WALLSEND[1]
-    else
-      closest_point = Vector[BOWNESS[0] + t * dx, BOWNESS[1] + t * dy]
-      dx = point[0] - closest[0]
-      dy = point[1] - closest[1]
-    end
-
-    distance_to_wall=Math.sqrt(dx * dx + dy * dy);
-
-    if distance_to_wall > 3.0
-      self.set_random_location!
-    else
-      self.location_on_wall = self.class.distance(closest_point, BOWNESS)
-    end
+    self.location_on_wall = HadriansWall.percent_along_the_wall([longitude, latitude])
   end
 
   def post_time
