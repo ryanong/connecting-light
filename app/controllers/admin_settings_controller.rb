7f936bf9cc4963b89bda2ae1acc41c54c2e4f1c2
Ryan Ong <ryanong@gmail.com>
2012-08-27 11:45:15 -0400

    optimize admin settings fetch

--- app/controllers/admin_settings_controller.rb
+++ app/controllers/admin_settings_controller.rb
@@ -6,15 +6,7 @@ class AdminSettingsController < ApplicationController
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json {
-        settings = {
-          time: Time.now.to_i
-        }
-        @admin_settings.each do |setting|
-          settings[setting.name] = setting.value
-        end
-        render json: settings
-      }
+      format.json { render json: AdminSetting.fetch.merge(time: Time.now.to_i) }
     end
   end
 
